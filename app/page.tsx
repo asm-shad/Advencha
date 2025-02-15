ba3722bfc2647df149103d161e9d474c186c553f
Asm Shad (INT) <asm.shad@liz.com>
2025-02-16 00:29:26 +0600

    Section Heading helper component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Destination from "@/components/Destination/Destination";
 import Home from "@/components/Home/Home";
 import React from "react";
 
@@ -5,6 +6,7 @@ function HomePage() {
   return (
     <div>
       <Home></Home>
+      <Destination></Destination>
     </div>
   );
 }
