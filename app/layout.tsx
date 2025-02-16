7eae2da6c43d426bbf3993790b587e68a29f9929
Asm Shad (INT) <asm.shad@liz.com>
2025-02-16 16:51:55 +0600

    ScrollToTop Button component created and succrssfully implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins, Rancho } from "next/font/google"; // Add Rancho here
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -48,6 +49,7 @@ export default function RootLayout({
         <ResponsiveNav></ResponsiveNav>
         {children}
         <Footer></Footer>
+        <ScrollToTop></ScrollToTop>
       </body>
     </html>
   );
