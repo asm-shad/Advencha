c779288c2e8fe6eb11837a4a1737811a2599c7a5
Asm Shad (INT) <asm.shad@liz.com>
2025-02-16 08:37:46 +0600

    Discover Popular Destinations Section Complete

--- components/Destination/Destination.tsx
+++ components/Destination/Destination.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import SectionHeading from "../Helper/SectionHeading";
+import DestinationSlider from "./DestinationSlider";
 
 const Destination = () => {
   return (
-    <div className=''>Destination</div>
-  )
-}
+    <div className="pt-20 pb-20">
+      <SectionHeading
+        heading="Discovering Popular Destinations"
+        paragraph="Explore stunning places filled with beauty, culture, and adventure. Your next journey awaits!"
+      ></SectionHeading>
+      {/* Section Content  */}
+      <div className="mt-14 w-[80%] mx-auto">
+      {/* Slider  */}
+      <DestinationSlider></DestinationSlider>
+      
+      </div>
+    </div>
+  );
+};
 
-export default Destination
+export default Destination;
